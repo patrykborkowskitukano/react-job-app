b542052dd52e76cbc6bb3361bb1680df18f2f60f
Patryk Borkowski <patryk.borkowski@tukano.com.pl>
2023-07-26 18:30:41 +0200

    Added files

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text style={{ fontSize: 20 }}>Home Screen</Text>
+        </View>
+    )
+}
+
+export default Home;
