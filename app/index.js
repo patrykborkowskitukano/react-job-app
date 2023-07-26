409824c3a2a5a26c0a6be0220a7e8a930e09db00
Patryk Borkowski <patryk.borkowski@tukano.com.pl>
2023-07-26 19:48:50 +0200

    asd

--- app/index.js
+++ app/index.js
@@ -2,11 +2,30 @@ import { useState } from 'react';
 import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text style={{ fontSize: 20 }}>Home Screen</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: {
+                        backgroundColor: COLORS.lightWhite,
+                    },
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    },
+                    headerRight: () => {
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                    },
+
+                }}
+            />
+        </SafeAreaView>
     );
 };
 
