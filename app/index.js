8a21ab09d43c5ddd1638af9899698d1259cc9432
Patryk Borkowski <patryk.borkowski@tukano.com.pl>
2023-07-26 19:48:38 +0200

    asd

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 
 const Home = () => {
     return (
         <View>
             <Text style={{ fontSize: 20 }}>Home Screen</Text>
         </View>
-    )
-}
+    );
+};
+
 
 export default Home;
